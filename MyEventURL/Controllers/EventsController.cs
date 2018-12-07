770726816057829c2fda2901ee970d77a5278feb
Avi Sujeeth <avisuj@hotmail.com>
2018-12-06 21:28:15 -0600

    EventDetails Progress

--- MyEventURL/Controllers/EventsController.cs
+++ MyEventURL/Controllers/EventsController.cs
@@ -36,59 +36,6 @@ namespace MyEventURL.Controllers
             return View(@event);
         }
 
-        // GET: Events/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Events/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EventId,Start,End,Title,Description,Location,Organizer,Email,Reminder,Format,Views,Engaged,sway,search,Timezone,Facebook,AllDay,Recurring,SocialCommentsEngine,TwitterDataWidget,YammerNetwork,SummaryEngine,youtube,storify,surveymonkey")] Event @event)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Events.Add(@event);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(@event);
-        }
-
-        // GET: Events/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Event @event = db.Events.Find(id);
-            if (@event == null)
-            {
-                return HttpNotFound();
-            }
-            return View(@event);
-        }
-
-        // POST: Events/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EventId,Start,End,Title,Description,Location,Organizer,Email,Reminder,Format,Views,Engaged,sway,search,Timezone,Facebook,AllDay,Recurring,SocialCommentsEngine,TwitterDataWidget,YammerNetwork,SummaryEngine,youtube,storify,surveymonkey")] Event @event)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(@event).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(@event);
-        }
 
         // GET: Events/Delete/5
         public ActionResult Delete(int? id)
